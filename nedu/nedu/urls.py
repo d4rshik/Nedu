074f94994d0b681e6904b492a138e671327d6acb
Darshik A S <d4rshik@gmail.com>
2019-05-21 10:26:50 +0530

    url path

--- nedu/nedu/urls.py
+++ nedu/nedu/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from home.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
